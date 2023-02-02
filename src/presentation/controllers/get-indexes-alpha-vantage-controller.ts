30cf09eecbf9c8a076075de5db0e89252e1179c1
Paulo Mendes <paulo.silva.contratada@lasa.com.br>
2023-02-02 02:45:04 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/get-indexes-alpha-vantage-controller.ts
+++ src/presentation/controllers/get-indexes-alpha-vantage-controller.ts
@@ -1,20 +1,15 @@
 import { HttpRequest, HttpResponse } from '../../../src/presentation/protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class GetIndexesAlphaVantageController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.api_key) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('api_key')
-      }
+      return badRequest(new MissingParamError('api_key'))
     }
 
     if (!httpRequest.body.function) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('function')
-      }
+      return badRequest(new MissingParamError('function'))
     }
 
     return {
