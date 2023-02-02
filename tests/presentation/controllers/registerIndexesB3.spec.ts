fcded2cd3aadeb508f399279583745738a3b5c30
Paulo Mendes <paulo.silva.contratada@lasa.com.br>
2023-02-02 02:18:22 -0300

    test: ensure GetIndexesAplhaVantageController returns 400 if no api key is provided

---
+++ tests/presentation/controllers/registerIndexesB3.spec.ts
@@ -0,0 +1,15 @@
+import { GetIndexesAlphaVantageController } from '../../../src/presentation/controllers/get-indexes-alpha-vantage-controller'
+
+describe('GetIndexesAlphaVantageController', () => {
+  test('Should return 400 if no api key is provided', () => {
+    const sut = new GetIndexesAlphaVantageController()
+    const httpRequest = {
+      body: {
+        function: 'any_function',
+        symbol: 'any_symbol'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
