d62551c75759ac097967555d2b4c1ae71a4aaf2b
Paulo Mendes <paulo.silva.contratada@lasa.com.br>
2023-02-02 02:28:56 -0300

    feat: ensure GetIndexesAplhaVantageController retruns 400 if no function is provided

--- src/presentation/controllers/get-indexes-alpha-vantage-controller.ts
+++ src/presentation/controllers/get-indexes-alpha-vantage-controller.ts
@@ -1,8 +1,17 @@
 export class GetIndexesAlphaVantageController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!httpRequest.body.api_key) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: api_key')
+      }
+    }
+
+    if (!httpRequest.body.function) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: function')
+      }
     }
   }
 }
