c36ae6cafbc3ff3a655baac69571c4e5400531b7
Paulo Mendes <paulo.silva.contratada@lasa.com.br>
2023-02-03 01:39:21 -0300

    feat: ensure GetIndexesB3Controller returns 500 if GetIndexes throws

--- src/presentation/controllers/get-indexes-b3-controller.ts
+++ src/presentation/controllers/get-indexes-b3-controller.ts
@@ -3,31 +3,39 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { IController } from '../interfaces/i-controller'
 import { IGetIndexes } from '../interfaces/i-get-indexes'
+import { ServerError } from '../errors/server-error'
 
 export class GetIndexesB3Controller implements IController {
   constructor (private readonly getIndexes: IGetIndexes) {}
 
   handle (httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFields = ['api_key', 'function', 'symbol']
+    try {
+      const requiredFields = ['api_key', 'function', 'symbol']
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    const indexes = this.getIndexes.request(httpRequest.body)
+      const indexes = this.getIndexes.request(httpRequest.body)
 
-    if (!indexes) {
+      if (!indexes) {
+        return {
+          statusCode: 400,
+          body: ''
+        }
+      }
+
       return {
-        statusCode: 400,
+        statusCode: 200,
         body: ''
       }
-    }
-
-    return {
-      statusCode: 200,
-      body: ''
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
